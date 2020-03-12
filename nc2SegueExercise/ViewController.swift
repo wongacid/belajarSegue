a1c84cdb1d053e572256a4ac91a6f5aa0d222595
Firza Ilhami <wongacid@gmail.com>
2020-03-12 16:14:59 +0700

    Initial Commit

---
+++ nc2SegueExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nc2SegueExercise
+//
+//  Created by Firza Ilhami on 12/03/20.
+//  Copyright © 2020 Firza Ilhami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
